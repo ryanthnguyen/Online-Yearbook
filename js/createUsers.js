7f83d5fe23529e7a30bb69930c1e75536ec8355d
ryanthnguyen <qtquoc@memphis.edu>
2017-07-03 15:16:46 -0500

    Need to implement Authentication

--- js/createUsers.js
+++ js/createUsers.js
@@ -8,9 +8,16 @@ var grade = document.getElementById('gradeBox');
 var phone = document.getElementById('phoneBox');
 
 
+
 signUpBtn.addEventListener('click', e => {
   var email = user.value + "@gmail.com";
   var passwordValue = password.value;
+  var firstNameValue = firstName.value;
+  var lastNameValue lastName.value;
+  var emailValue = actuallyEmail.value;
+  var gradeValue = grade.value;
+  var phoneValue = phone.value;
+  var databaseRef = firebase.database().ref().child('Users');
   var auth = firebase.auth();
   var promise = auth.createUserWithEmailAndPassword(email,passwordValue);
   promise.catch(e => console.log(e.message));
