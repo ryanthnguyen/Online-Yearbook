45cf087ce1691caaffb6f347da927595322ef932
ryanthnguyen <qtquoc@memphis.edu>
2017-07-04 12:04:28 -0500

    need to debug registration form

--- js/createUsers.js
+++ js/createUsers.js
@@ -3,22 +3,44 @@ var password = document.getElementById('passwordBox');
 var signUpBtn = document.getElementById('signUpButton');
 var firstName = document.getElementById('firstNameBox');
 var lastName = document.getElementById('lastNameBox');
-var actuallyEmail = document.getElementById('emailBox');
+var email = document.getElementById('emailBox');
 var grade = document.getElementById('gradeBox');
 var phone = document.getElementById('phoneBox');
-
-
-
+var firebaseRef = firebase.database().ref();
 signUpBtn.addEventListener('click', e => {
-  var email = user.value + "@gmail.com";
+  console.log('Running fine');
+  var email = username.value + "@gmail.com";
   var passwordValue = password.value;
-  var firstNameValue = firstName.value;
-  var lastNameValue lastName.value;
-  var emailValue = actuallyEmail.value;
-  var gradeValue = grade.value;
-  var phoneValue = phone.value;
-  var databaseRef = firebase.database().ref().child('Users');
+  var usernameValue = username.value;
   var auth = firebase.auth();
   var promise = auth.createUserWithEmailAndPassword(email,passwordValue);
   promise.catch(e => console.log(e.message));
+});
+
+
+firebase.auth().onAuthStateChanged(function(user){
+  if (user) {
+    var currentUser = user.uid;
+    var paswordValue = password.value;
+    var firstNameValue = firstName.value;
+    var lastNameValue = lastName.value;
+    var emailValue = email.value;
+    var gradeValue = grade.value;
+    var phoneValue = phone.value;
+      currentUser = {
+      'Username': usernameValue,
+      Password: paswordValue,
+      'First Name': firstNameValue,
+      'Last Name': lastNameValue,
+      Email: emailValue,
+      Grade: gradeValue,
+      'Phone Number': phoneValue
+    }
+    firebaseRef.push({
+
+    }
+    }
+  } else {
+    console.log('unsuccessful');
+  }
 });
