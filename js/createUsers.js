3e034e39b57b386bdb9788d2d32369cc73be49e3
ryanthnguyen <qtquoc@memphis.edu>
2017-07-04 15:40:07 -0500

    added changes

--- js/createUsers.js
+++ js/createUsers.js
@@ -1,3 +1,5 @@
+// TODO: make it so how everytime the page is closed or restart the user is sign out
+//Bug: everytime the user close the browser it still sign them in
 var username = document.getElementById('usernameBox');
 var password = document.getElementById('passwordBox');
 var signUpBtn = document.getElementById('signUpButton');
@@ -6,41 +8,34 @@ var lastName = document.getElementById('lastNameBox');
 var email = document.getElementById('emailBox');
 var grade = document.getElementById('gradeBox');
 var phone = document.getElementById('phoneBox');
-var firebaseRef = firebase.database().ref();
+var firebaseRef = firebase.database().ref('Users');
 signUpBtn.addEventListener('click', e => {
-  console.log('Running fine');
-  var email = username.value + "@gmail.com";
-  var passwordValue = password.value;
   var usernameValue = username.value;
+  var passwordValue = password.value;
+  var email = usernameValue + "@gmail.com";
   var auth = firebase.auth();
   var promise = auth.createUserWithEmailAndPassword(email,passwordValue);
-  promise.catch(e => console.log(e.message));
+  promise
+    .then(user => console.log(user));
 });
-
 
 firebase.auth().onAuthStateChanged(function(user){
   if (user) {
-    var currentUser = user.uid;
-    var paswordValue = password.value;
-    var firstNameValue = firstName.value;
-    var lastNameValue = lastName.value;
+    var currentuser = firebase.auth().currentUser;
+    var firstnameValue = firstName.value;
+    var lastnameValue = lastName.value;
     var emailValue = email.value;
     var gradeValue = grade.value;
     var phoneValue = phone.value;
-      currentUser = {
-      'Username': usernameValue,
-      Password: paswordValue,
-      'First Name': firstNameValue,
-      'Last Name': lastNameValue,
-      Email: emailValue,
+    firebaseRef.child(currentuser.uid).set({
+      'First Name': firstnameValue,
+      'Last Name': lastnameValue,
+      Email: email.value,
       Grade: gradeValue,
-      'Phone Number': phoneValue
-    }
-    firebaseRef.push({
-
-    }
-    }
+      Phone: phoneValue
+    });
+    console.log('User is' + user);
   } else {
-    console.log('unsuccessful');
+    console.log('User is null');
   }
-});
+})
